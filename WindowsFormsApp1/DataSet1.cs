2c8d81f4192dc3a8ca2df1246cb7d1d05eed1159
Russell-Smith <rs03600@GeorgiaSouthern.edu>
2017-11-11 14:14:01 -0500

    updated some backend

--- WindowsFormsApp1/DataSet1.cs
+++ WindowsFormsApp1/DataSet1.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp1 {
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp1 {
     
     
     public partial class DataSet1
@@ -7,5 +9,26 @@
         {
 
         }
+    }
+        //the class that manages the cards within the queues, and each queue.
+        //constructs them in UI as well
+    public class QueueManager
+    {
+
+        
+        //when a card is removed, remove the bottom UI card and shift all info up.
+        List <QueueCard> queue1 = new List<QueueCard>(); //high queue priority
+        List<QueueCard> queue2 = new List<QueueCard>(); //medium queue priority
+        List<QueueCard> queue3 = new List<QueueCard>(); //low queue priority
+
+        
+    }
+        //the class that creates and maintains the cards information.
+    public class QueueCard
+    {
+        string artistName, comissionerName, note;
+        int queuePriority, queuePoisition, complexity;
+        float price;
+
     }
 }
